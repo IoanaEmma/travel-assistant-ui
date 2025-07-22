016826dd483ef8acb11e80534e25daa6922a2fdb
Ioana <ioana.ciangau@siemens.com>
2025-07-22 14:13:44 +0300

    Add flight and attraction reducers and APIs to the store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,10 @@ import hotelReducer from '../features/hotel/hotelSlice';
 import { hotelApi } from '../features/hotel/hotelApi';
 import tripReducer from '../features/trip/tripSlice';
 import { tripApi } from '../features/trip/tripApi';
+import flightReducer from '../features/flight/flightSlice';
+import { flightApi } from '../features/flight/flightApi';
+import attractionReducer from '../features/attractions/attractionSlice';
+import { attractionApi } from '../features/attractions/attractionApi';
 
 export const store = configureStore({
     reducer: {
@@ -14,9 +18,14 @@ export const store = configureStore({
         [hotelApi.reducerPath]: hotelApi.reducer,
         trip: tripReducer,
         [tripApi.reducerPath]: tripApi.reducer,
+        flight: flightReducer,
+        [flightApi.reducerPath]: flightApi.reducer,
+        attraction: attractionReducer,
+        [attractionApi.reducerPath]: attractionApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(travelApi.middleware, hotelApi.middleware, tripApi.middleware)
+        getDefaultMiddleware().concat(travelApi.middleware, hotelApi.middleware,
+            tripApi.middleware, flightApi.middleware, attractionApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
